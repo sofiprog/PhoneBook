8d4b9a06c9b221a44429b210dcb9918bc12bae45
sofiprog <terka_2018@mail.ru>
2020-08-18 13:32:49 +0300

    Добавление выхода из программы, обработки неправильного выбора режима

--- PhoneBook.py
+++ PhoneBook.py
@@ -13,23 +13,23 @@ phone_book = dict()
 
 welcome()
 
-menu()
-choice = int(input("Введите режим работы: "))
+while True:
+    menu()
+    choice = int(input("Введите режим работы: "))
 
-if choice == 1:
-    print(phone_book)
-elif choice == 2:
-    tel = input("Введите номер телефона: ")
-    value = input_data()
-
-    phone_book[tel] = value
-elif choice == 3:  # TODO Редактирование записи
-    print()
-elif choice == 4:  # TODO удаление записи
-    print()
-elif choice == 0:  # TODO выход
-    print()
-else:
-    print("Неправильный режим")
-
-print(phone_book)
+    if choice == 1:
+        print(phone_book)
+    elif choice == 2:    # TODO сделать проверку существования телефона в справочнике
+        tel = input("Введите номер телефона: ")
+        value = input_data()
+        phone_book[tel] = value
+    elif choice == 3:   # TODO Редактирование записи
+        print()
+    elif choice == 4:   # TODO удаление записи
+        print()
+    elif choice == 0:   
+        print("До свидания")
+        break
+    else:
+        print("Неправильный режим")
+        continue
